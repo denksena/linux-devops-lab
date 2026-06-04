c4dad27a2a127a0a7c464809b9d1de9cc3146fb3
denksena <poruchik.rgev@gmail.com>
2026-06-05 00:52:30 +0400

    Add Flask-PostgreSQL connection via Docker Compose and env config

--- app/app.py
+++ app/app.py
@@ -1,10 +1,30 @@
 from flask import Flask
+import os
+import psycopg2
 
 app = Flask(__name__)
 
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=5432
+    )
+
 @app.route('/')
 def home():
-    return "DevOps mini project works!"
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT version();")
+    db_version = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return f"DB connected: {db_version[0]}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
